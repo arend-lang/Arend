b5f58cb7c3b8fafe4271750de6eee14960efb7aa
valis <valery.isaev@gmail.com>
2025-02-07 11:07:07 +0300

    Fix meta definitions

--- base/src/main/java/org/arend/naming/resolving/visitor/DefinitionResolveNameVisitor.java
+++ base/src/main/java/org/arend/naming/resolving/visitor/DefinitionResolveNameVisitor.java
@@ -83,7 +83,9 @@ public class DefinitionResolveNameVisitor implements ConcreteResolvableDefinitio
       def.body = def.body.accept(exprVisitor, null);
     }
 
-    def.getData().setDefinition(def);
+    if (def.getData().getDefinition() instanceof DefinableMetaDefinition) {
+      def.getData().setDefinition(def);
+    }
     def.setResolved();
     SyntacticDesugarVisitor.desugar(def, myLocalErrorReporter);
     if (myResolverListener != null) {
