8386dc3246378c2a3c4f2d22a8b406468f795bee
sxhya <sinchukss@gmail.com>
2025-04-02 18:47:50 +0200

    Fix ArendExtendWordSelectionTest

--- intellij/src/test/kotlin/org/arend/actions/ArendExtendWordSelectionTest.kt
+++ intellij/src/test/kotlin/org/arend/actions/ArendExtendWordSelectionTest.kt
@@ -6,6 +6,7 @@ import org.arend.ArendTestBase
 class ArendExtendWordSelectionTest: ArendTestBase() {
     fun doTest(before: String, finalRange: String, count: Int = 1) {
         InlineFile(before).withCaret()
+        myFixture.doHighlighting()
         for (i in 1..count) myFixture.performEditorAction(IdeActions.ACTION_EDITOR_SELECT_WORD_AT_CARET)
         val selectionModel = myFixture.editor.selectionModel
         val t = myFixture.file.text.substring(selectionModel.selectionStart, selectionModel.selectionEnd)
