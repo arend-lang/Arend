a9ba8a9a2b5d5eea04cd040615c059dbcb916185
valis <valery.isaev@gmail.com>
2025-06-24 18:59:38 +0300

    Implement error messages

--- arend-lib/meta/src/main/java/org/arend/lib/error/equation/GroupNFPrettyPrinter.java
+++ arend-lib/meta/src/main/java/org/arend/lib/error/equation/GroupNFPrettyPrinter.java
@@ -5,11 +5,9 @@ import org.arend.ext.prettyprinting.doc.LineDoc;
 import org.arend.ext.util.Pair;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import static org.arend.ext.prettyprinting.doc.DocFactory.hSep;
 import static org.arend.ext.prettyprinting.doc.DocFactory.text;
 
 public class GroupNFPrettyPrinter implements NFPrettyPrinter<List<Pair<Boolean,Integer>>> {
@@ -19,18 +17,36 @@ public class GroupNFPrettyPrinter implements NFPrettyPrinter<List<Pair<Boolean,I
     this.isMultiplicative = isMultiplicative;
   }
 
+  private void printPair(boolean isPos, int var, List<Pair<String, CoreExpression>> names, StringBuilder builder) {
+    String name = names.get(var).proj1;
+    if (isPos) {
+      builder.append(name);
+    } else {
+      builder.append(isMultiplicative ? "" : "-").append(name).append(isMultiplicative ? "^-1" : "");
+    }
+  }
+
   @Override
   public @NotNull LineDoc nfToDoc(@NotNull List<Pair<Boolean,Integer>> nf, @NotNull List<Pair<String, CoreExpression>> names) {
     if (nf.isEmpty()) {
       return text(isMultiplicative ? "1" : "0");
     }
 
-    List<LineDoc> docs = new ArrayList<>(nf.size());
-    for (var pair : nf) {
-      String name = names.get(pair.proj2).proj1;
-      docs.add(text(pair.proj1 ? name : (isMultiplicative ? "" : "-") + name + (isMultiplicative ? "^-1" : "")));
+    StringBuilder builder = new StringBuilder();
+    printPair(nf.getFirst().proj1, nf.getFirst().proj2, names, builder);
+
+    for (int i = 1; i < nf.size(); i++) {
+      var pair = nf.get(i);
+      if (isMultiplicative) {
+        builder.append(" * ");
+        printPair(pair.proj1, pair.proj2, names, builder);
+      } else {
+        builder.append(pair.proj1 ? " + " : " - ");
+        printPair(true, pair.proj2, names, builder);
+      }
     }
-    return hSep(text(isMultiplicative ? " * " : " + "), docs);
+
+    return text(builder.toString());
   }
 
   @Override
--- arend-lib/meta/src/main/java/org/arend/lib/error/equation/RingNFPrettyPrinter.java
+++ arend-lib/meta/src/main/java/org/arend/lib/error/equation/RingNFPrettyPrinter.java
@@ -7,24 +7,50 @@ import org.arend.lib.ring.Monomial;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import static org.arend.ext.prettyprinting.doc.DocFactory.*;
 
 public class RingNFPrettyPrinter implements NFPrettyPrinter<List<Monomial>> {
+  private void printMonomial(Monomial monomial, List<Pair<String, CoreExpression>> names, StringBuilder builder) {
+    List<Integer> elements = monomial.elements();
+    if (elements.isEmpty()) {
+      builder.append(monomial.coefficient().toString());
+    } else {
+      if (monomial.coefficient().equals(BigInteger.ONE)) {
+        builder.append(names.get(elements.getFirst()).proj1);
+      } else if (monomial.coefficient().equals(BigInteger.ONE.negate())) {
+        builder.append("-").append(names.get(elements.getFirst()).proj1);
+      } else {
+        builder.append(monomial.coefficient()).append(" * ").append(names.get(elements.getFirst()).proj1);
+      }
+      for (int i = 1; i < elements.size(); i++) {
+        builder.append(" * ").append(names.get(elements.get(i)).proj1);
+      }
+    }
+  }
+
   @Override
   public @NotNull LineDoc nfToDoc(@NotNull List<Monomial> poly, @NotNull List<Pair<String, CoreExpression>> names) {
     if (poly.isEmpty()) {
       return text("0");
     }
 
-    List<LineDoc> docs = new ArrayList<>(poly.size());
-    for (Monomial monomial : poly) {
-      docs.add(monomial.elements().isEmpty() ? text(monomial.coefficient().toString()) : hList(monomial.coefficient().equals(BigInteger.ONE) ? empty() : text(monomial.coefficient() + " * "), hSep(text(" * "), monomial.elements().stream().map(var -> text(names.get(var).proj1)).toList())));
+    StringBuilder builder = new StringBuilder();
+    printMonomial(poly.getFirst(), names, builder);
+    for (int i = 1; i < poly.size(); i++) {
+      Monomial monomial = poly.get(i);
+      if (monomial.coefficient().signum() < 0) {
+        builder.append(" - ");
+        printMonomial(new Monomial(monomial.coefficient().negate(), monomial.elements()), names, builder);
+      } else {
+        builder.append(" + ");
+        printMonomial(monomial, names, builder);
+      }
     }
-    return hSep(text(" + "), docs);
+
+    return text(builder.toString());
   }
 
   @Override
