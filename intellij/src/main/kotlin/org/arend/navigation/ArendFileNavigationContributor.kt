47660fee2dd37cef36985104c7c61d77f95386f3
valis <valery.isaev@gmail.com>
2025-09-05 19:21:21 +0300

    Update ArendFileNavigationContributor

--- intellij/src/main/kotlin/org/arend/navigation/ArendFileNavigationContributor.kt
+++ intellij/src/main/kotlin/org/arend/navigation/ArendFileNavigationContributor.kt
@@ -5,24 +5,17 @@ import com.intellij.navigation.NavigationItem
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.util.containers.addIfNotNull
+import org.arend.ext.module.ModuleLocation
 import org.arend.ext.module.ModulePath
-import org.arend.module.config.LibraryConfig
-import org.arend.prelude.Prelude
+import org.arend.ext.reference.DataContainer
 import org.arend.server.ArendServerService
 import org.arend.util.FileUtils
-import org.arend.util.findLibrary
 
 class ArendFileNavigationContributor : ChooseByNameContributor {
     override fun getNames(project: Project?, includeNonProjectItems: Boolean): Array<String> {
         project ?: return emptyArray()
-        val result = mutableListOf(Prelude.MODULE_PATH.toString() + FileUtils.EXTENSION)
-        val service = project.service<ArendServerService>()
-        forEachArendRawLib(service) { lib ->
-            lib.additionalModulesSet.forEach { modulePath ->
-                result.add(modulePath.toString() + FileUtils.EXTENSION)
-            }
-        }
-        return result.toTypedArray()
+        return project.service<ArendServerService>().server.modules.filter { it.locationKind == ModuleLocation.LocationKind.GENERATED }.map { it.modulePath.toString() + FileUtils.EXTENSION }.toTypedArray()
     }
 
     override fun getItemsByName(
@@ -34,22 +27,12 @@ class ArendFileNavigationContributor : ChooseByNameContributor {
         project ?: return emptyArray()
         name ?: return emptyArray()
         val result = mutableListOf<NavigationItem>()
-        if (name == Prelude.MODULE_PATH.toString() + FileUtils.EXTENSION) {
-            project.service<ArendServerService>().prelude?.let { result.add(it) }
-        }
         val modulePath = ModulePath.fromString(FileUtil.getNameWithoutExtension(name))
-        val service = project.service<ArendServerService>()
-        forEachArendRawLib(service) { lib ->
-            lib.findArendFile(modulePath, withAdditional = true, withTests = false)?.let { file ->
-                result.add(file)
-            }
+        val server = project.service<ArendServerService>().server
+        for (lib in server.libraries) {
+            val group = server.getRawGroup(ModuleLocation(lib, ModuleLocation.LocationKind.GENERATED, modulePath))
+            result.addIfNotNull((group?.referable as? DataContainer)?.data as? NavigationItem)
         }
         return result.toTypedArray()
-    }
-}
-
-private fun forEachArendRawLib(service: ArendServerService, action: (LibraryConfig) -> Unit) {
-    for (library in service.server.libraries) {
-        service.project.findLibrary(library)?.let(action)
     }
 }
