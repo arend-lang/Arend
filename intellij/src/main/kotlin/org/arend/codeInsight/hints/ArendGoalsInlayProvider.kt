53e0b92843fba23a8b227cc794a1f8904bd8a5a1
Aleksei Luchinin <alex999990009@gmail.com>
2025-09-15 14:16:03 +0200

    Changed the inlay hints to declarative ones

--- intellij/src/main/kotlin/org/arend/codeInsight/hints/ArendGoalsInlayProvider.kt
+++ intellij/src/main/kotlin/org/arend/codeInsight/hints/ArendGoalsInlayProvider.kt
@@ -1,27 +1,8 @@
 package org.arend.codeInsight.hints
 
-import com.intellij.codeInsight.hints.NoSettings
-import com.intellij.codeInsight.hints.SettingsKey
 import org.arend.core.definition.Definition
 
-@Suppress("UnstableApiUsage")
 class ArendGoalsInlayProvider : ArendDefinitionInlayProvider() {
-    override val key: SettingsKey<NoSettings>
-        get() = SettingsKey("arend.inlays.goals")
-
-    override val name: String
-        get() = "Goals"
-
-    override val previewText: String
-        get() = """
-            \func foo => {?}
-            
-            \func bar => foo
-        """.trimIndent()
-
-    override val description
-        get() = "Shows definitions with goals used by a definition"
-
     override fun getText(definition: Definition): String? {
         val goals = HashSet(definition.goals)
         goals.remove(definition)
