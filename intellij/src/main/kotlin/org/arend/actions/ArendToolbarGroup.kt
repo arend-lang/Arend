151774c77a36bc61a3cb988bd0e80e94afc25968
Aleksei Luchinin <alex999990009@gmail.com>
2025-09-16 11:11:31 +0200

    Fix the MainToolBar exception

--- intellij/src/main/kotlin/org/arend/actions/ArendToolbarGroup.kt
+++ intellij/src/main/kotlin/org/arend/actions/ArendToolbarGroup.kt
@@ -1,18 +1,23 @@
 package org.arend.actions
 
 import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.project.Project
 import org.arend.toolWindow.errors.ArendPrintOptionsActionGroup
 import org.arend.toolWindow.errors.PrintOptionKind
 
 class ArendToolbarGroup : ActionGroup() {
-  override fun getChildren(eNull: AnActionEvent?): Array<AnAction> {
+    private val actions = mutableMapOf<Project, List<AnAction>>()
+
+    override fun getChildren(eNull: AnActionEvent?): Array<AnAction> {
     val e = eNull ?: return emptyArray()
     val project = e.project ?: return emptyArray()
     if (project.isDisposed) return emptyArray()
+    if (actions[project] == null) {
+        actions.put(project, listOf(ArendNormalizeToggleAction, ArendPrintOptionsActionGroup(project, PrintOptionKind.POPUP_PRINT_OPTIONS)))
+    }
     return arrayOf(
         Separator.getInstance(),
-        ArendNormalizeToggleAction,
-        ArendPrintOptionsActionGroup(project, PrintOptionKind.POPUP_PRINT_OPTIONS),
+        *(actions[project]?.toTypedArray() ?: emptyArray()),
         Separator.getInstance()
     )
   }
