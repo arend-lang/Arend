2513793aaa5179f54c7bfdd6398f3ea10f6d5c38
Artem Semidetnov <artemsemidetnov@gmail.com>
2026-04-01 18:17:28 +0200

    add SearchSymbolsTool for symbol searching by name pattern in Arend modules, update MCP tools registry

--- intellij/src/main/kotlin/org/arend/aifeatures/mcpTools/ShowModulesTool.kt
+++ intellij/src/main/kotlin/org/arend/aifeatures/mcpTools/ShowModulesTool.kt
@@ -1,18 +1,16 @@
 package org.arend.aifeatures.mcpTools
 
-import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import kotlinx.serialization.json.putJsonObject
 import org.arend.aifeatures.McpTool
+import org.arend.aifeatures.mcpTools.common.numberOfAllowedLines
 import org.arend.aifeatures.mcpTools.common.outputFile
 import org.arend.aifeatures.parseDataWithModules
 import org.arend.ext.module.ModuleLocation
 import org.arend.ext.module.ModulePath
-import org.arend.server.ArendServerService
-import org.arend.term.group.ConcreteGroup
 
 class ShowModulesTool : McpTool {
   override val name: String = "ShowModules"
@@ -24,8 +22,8 @@ class ShowModulesTool : McpTool {
   override val description: String = "Shows the content of a given file. " +
     "You must send it the full name of the file (module) as a string. " +
     "Then, the tool will return the content of this file. " +
-    "If the file is bigger than 500 lines, only the first 500 lines will be returned." +
-    "In the case the file is larger than 500 lines, use another tool that shows the content of the file starting from a given line."
+    "If the file is bigger than $numberOfAllowedLines lines, only the first $numberOfAllowedLines lines will be returned." +
+    "In the case the file is larger than $numberOfAllowedLines lines, use another tool that shows the content of the file starting from a given line."
 
   override fun getInputSchema(): JsonObject =
     buildJsonObject {
@@ -39,20 +37,34 @@ class ShowModulesTool : McpTool {
 
   override fun execute(project: Project, arguments: String): String {
     val parsedUserRequest = parseDataWithModules(arguments)
+    
+    if (parsedUserRequest.modulePaths.isEmpty()) {
+      return "Error: No module path provided"
+    }
+    
     val module = parsedUserRequest.modulePaths.first()
     val libPath = parsedUserRequest.libPath
+    
+    if (libPath.isBlank()) {
+      return "Error: No library path provided"
+    }
+    
     val libraryName = java.io.File(libPath).name
     val path = ModulePath.fromString(module.split("/").last())
     val sourceLocation = ModuleLocation(libraryName, ModuleLocation.LocationKind.SOURCE, path)
-    println("parsedUserRequest: $parsedUserRequest")
 
-
-    val outputFile : String = outputFile(project, sourceLocation)
-    println("outputFile: $outputFile")
-    if (outputFile.split("\n").size <= 500){
-      return "Showing the whole file:\n \"$outputFile"
+    val fileContent = outputFile(project, sourceLocation)
+    
+    if (fileContent.startsWith("Error:")) {
+      return fileContent
     }
-    return "Showing the first 500 lines of the file:\n \"$outputFile"
-
+    
+    val lines = fileContent.split("\n")
+    if (lines.size <= numberOfAllowedLines) {
+      return "Showing the whole file:\n\"$fileContent\""
+    }
+    
+    val truncatedContent = lines.take(numberOfAllowedLines).joinToString("\n")
+    return "Showing the first $numberOfAllowedLines lines of the file:\n\"$truncatedContent\""
   }
 }
